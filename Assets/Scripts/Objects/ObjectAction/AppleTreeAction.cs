95566a7bc8fedd8fc323719cb00a9fa7c0934c5a
Chaku <cristiangalindo13@gmail.com>
2022-08-23 16:32:51 +0200

    Apple tree ok

--- Assets/Scripts/Objects/ObjectAction/AppleTreeAction.cs
+++ Assets/Scripts/Objects/ObjectAction/AppleTreeAction.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class AppleTreeAction : MonoBehaviour, IObjectInteractable
 {
+    public GameObject appleObject;
+    public Transform appleSpawn;
+
+    private Animator _animator;
+    private bool _Droped = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     public void OnInteract()
     {
-        Debug.Log("Me han golpeado");
+        if(_Droped == false)
+        {
+            _animator.SetBool("Hited", true);
+            Instantiate(appleObject, appleSpawn);
+            _Droped = true;
+        }
     }
 }
