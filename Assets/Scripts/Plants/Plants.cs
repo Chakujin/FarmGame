ec0e79380cf995043b2d878ae69c6464956ec65a
Chakujin <cristiangalindo13@gmail.com>
2022-08-26 18:08:25 +0200

    Plant

--- Assets/Scripts/Plants/Plants.cs
+++ Assets/Scripts/Plants/Plants.cs
@@ -7,9 +7,8 @@ public class Plants : MonoBehaviour
     public Sprite[] plantIncrease;
     public SpriteRenderer myRenderer;
     public GameObject Fruit;
-    public Transform instantiatePoint;
 
-    private float f_currenTime;
+    [SerializeField]private float f_currenTime;
     public float growUp;
     private bool b_growUpPlant = true;
     private int i_growPlant = 0;
@@ -26,27 +25,37 @@ public class Plants : MonoBehaviour
         {
             f_currenTime += Time.deltaTime;
 
-            if (growUp >= f_currenTime)
+            if (growUp <= f_currenTime)
             {
                 b_growUpPlant = false;
             }
-        }
-        else if(b_growUpPlant == false && i_growPlant <= plantIncrease.Length)
-        {
-            DropFruit();
         }
     }
 
     public void OnPlantInteract()
     {
-        if (i_growPlant > plantIncrease.Length)// if my plant can growUp
+        if (b_growUpPlant == false)
         {
-            //Update sprite image
-            i_growPlant++;
-            myRenderer.sprite = plantIncrease[i_growPlant];
+            if (i_growPlant < plantIncrease.Length)// if my plant can growUp
+            {
+                //Update sprite image
+                i_growPlant++;
 
-            // enable continue growUp
-            b_growUpPlant = true;
+                if (i_growPlant == plantIncrease.Length)
+                {
+                    DropFruit();
+                }
+                else
+                {
+                    myRenderer.sprite = plantIncrease[i_growPlant];
+                }
+
+                // enable continue growUp
+                b_growUpPlant = true;
+                f_currenTime = 0f;
+
+
+            }
         }
     }
 
@@ -58,7 +67,11 @@ public class Plants : MonoBehaviour
         i_growPlant--;
         myRenderer.sprite = plantIncrease[i_growPlant];
 
+        float randomx = Random.Range(-2f,2f);
+        float randomy = Random.Range(-2f,2f);
+        Vector2 instantiatePoint = new Vector2(transform.position.x + randomx,transform.position.y + randomy);
+        Debug.Log(instantiatePoint);
         //Instantiate Fruit
-        Instantiate(Fruit, instantiatePoint);
+        Instantiate(Fruit, instantiatePoint,transform.localRotation);
     }
 }
